4c643aa5f498332812830bc76e80968a7724184a
jameswarreninss <turkeyoverlord@gmail.com>
2019-11-21 11:14:44 +0000

    change to jenkinsfile job

--- test.groovy
+++ test.groovy
@@ -1,27 +1,15 @@
 library 'INSSSharedLib'
 
-//Environment we are deploying to
-def environment = JobSuffix()[-3..-1].toUpperCase()
-//Environment we are running jenkins from
-def runEnvironment = GetEnvironment()
-
 def playprefix = 'UKC'
 def channel = '#operations'
 
 def project = 'inss'
-def previousjob = PreviousJob()
-
-def initmap = [
-    [projectname: project,
-    ChangeLogFile: 'job_change_log.log',
-    ChangeLogs: CollectChangeLogs()],
-]
 
 def gitmap = [
     [name: 'Repository', url: 'git@github.com:jameswarreninss/myrepotocheckout.git', poll: true, credentials: ''],
 ]
 
 node('master') {
-    StageLinearInOne ('BuildInitialise',initmap)
+    cleanWs()
     StageLinearInOne ('ScmCheckout',gitmap)
 }
